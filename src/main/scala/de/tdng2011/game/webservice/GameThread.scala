b907cb22b48d08ac9e1496382f960514754f5e63
Felix Becker <git@felixbecker.name>
2011-01-08 21:19:07 +0100

    thread timings korrigiert

--- src/main/scala/de/tdng2011/game/webservice/GameThread.scala
+++ src/main/scala/de/tdng2011/game/webservice/GameThread.scala
@@ -1,15 +1,15 @@
 package de.tdng2011.game.webservice
 
+import java.util.Date
 import de.tdng2011.game.kernel._
 import de.tdng2011.game.visual._
 import javax.swing.JFrame
 class GameThread extends Runnable {
-  override def run() {
+	val frameDuration = 25 
+	override def run() {
     // server start
     // visualizer start
-    val visualizer = CrystalBall
-    val thisThread = new Thread(visualizer);
-		thisThread start
+    new Thread(CrystalBall).start
 
     val m1 = Game.createMonster("test1", "8.8.8.8")
     val m2 = Game.createMonster("fun", "127.0.0.1")
@@ -17,23 +17,35 @@ class GameThread extends Runnable {
     val m4 = Game.createMonster("thorsten", "10.10.10.10")
     val m5 = Game.createMonster("supert0x", "18.18.18.18")
     val m6 = Game.createMonster("makubi", "16.16.16.16")
+    
+    var lastSleepTime : Double = 0
 
     while (true) {
-      Thread.sleep(100)
+      val frameStart = getTime
       Game.monsterAction(m1, false, false, false, false)
       Game.monsterAction(m2, false, true, true, false)
       Game.monsterAction(m3, true, true, true, true)
       Game.monsterAction(m4, false, false, true, false)     
       Game.monsterAction(m5, false, false, true, true)
       Game.monsterAction(m6, false, false, true, false)
-
-      Game think(100 / 1000.0)
+      Game think(lastSleepTime/1000)
+      
       
       val world : World = Game.getWorld
       
-      println(world)
+      CrystalBall setAllMonsters world
+      val frameEnd = getTime
+      val sleepTime =  frameDuration - (frameEnd - frameStart)
+      if(sleepTime > 0){
+    	  Thread sleep(sleepTime)
+    	  lastSleepTime = sleepTime
+      } else {
+    	  lastSleepTime = 0
+      }
       
-      visualizer setAllMonsters world
+      
     }
   }
+	
+	def getTime = new Date().getTime
 }
