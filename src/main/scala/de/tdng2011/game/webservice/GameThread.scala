bd5b19b6fdb187f91288560565484852c3589ee6
Mathias Kub <git@makubi.at>
2011-01-08 20:33:17 +0100

    Visualizer seperated from main thread.

--- src/main/scala/de/tdng2011/game/webservice/GameThread.scala
+++ src/main/scala/de/tdng2011/game/webservice/GameThread.scala
@@ -7,14 +7,16 @@ class GameThread extends Runnable {
   override def run() {
     // server start
     // visualizer start
-	  val visualizer = CrystalBall
-	  
+    val visualizer = CrystalBall
+    val thisThread = new Thread(visualizer);
+		thisThread start
 
     val m1 = Game.createMonster("test1", "8.8.8.8")
     val m2 = Game.createMonster("fun", "127.0.0.1")
     val m3 = Game.createMonster("felix", "123.123.123.123")
     val m4 = Game.createMonster("thorsten", "10.10.10.10")
     val m5 = Game.createMonster("supert0x", "18.18.18.18")
+    val m6 = Game.createMonster("makubi", "16.16.16.16")
 
     while (true) {
       Thread.sleep(100)
@@ -23,13 +25,14 @@ class GameThread extends Runnable {
       Game.monsterAction(m3, true, true, true, false)
       Game.monsterAction(m4, false, false, true, false)
       Game.monsterAction(m5, false, false, true, false)
+      Game.monsterAction(m6, false, false, true, false)
 
-      Game think(100 / 1000.0)
-      
-      val world : World = Game.getWorld
+      Game think (100 / 1000.0)
+
+      val world: World = Game.getWorld
       println(world)
-      
+
       visualizer setAllMonsters world
-      }
+    }
   }
 }
