e51fd0228473411e34a3bcda5307db24be5890b3
Benjamin Neff <benjamin@coding4coffee.ch>
2011-01-08 19:41:01 +0100

    shots in json

--- src/main/scala/de/tdng2011/game/kernel/World.scala
+++ src/main/scala/de/tdng2011/game/kernel/World.scala
@@ -28,7 +28,7 @@ case class World(monsters : List[Monster]) extends JsonSerializable {
 	}
 	
 	def findShot(id:String) = monsters.find(_.isShotFrom(id)).isDefined
-	def toJson() = "{\"monsters\":" + Json.build(monsters) + "}"
+	def toJson() = "{\"monsters\":" + Json.build(monsters.filter(!_.isShot)) + ",\"shots\":" + Json.build(monsters.filter(_.isShot)) + "}"
 	
 	override def toString = "Monsters:\n" + monsters
 }
