e9bd813e39fc9d2624363c1e5782c177dd6d0dc6
Oliver Jucknath <oliver@jucknath.de>
2011-01-08 23:23:49 +0100

    shots work

--- src/main/scala/de/tdng2011/game/kernel/WorldDefs.scala
+++ src/main/scala/de/tdng2011/game/kernel/WorldDefs.scala
@@ -5,8 +5,8 @@ import scala.math._
 object WorldDefs {
 	val size           =1000 //m
 	val speed          =100  //m/s
-	val shotSpeed      =speed*2
-	val shotTimeToLife =size/shotSpeed*0.2 
+	val shotSpeed      =speed*4
+	val shotTimeToLife =size/shotSpeed*0.5 
 	val rotSpeed       =2*Pi //rad/s
 	val monsterSize    =10
 
