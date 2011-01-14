5334134e598b2d003cef3baf838276ba46b8a09e
Benjamin Neff <benjamin@coding4coffee.ch>
2011-01-14 02:31:11 +0100

    fix compile error

--- src/main/scala/de/tdng2011/game/webservice/StartupListener.scala
+++ src/main/scala/de/tdng2011/game/webservice/StartupListener.scala
@@ -1,13 +1,13 @@
 package de.tdng2011.game.webservice
 
+import de.tdng2011.game.kernel.GameThread
 import javax.swing.JFrame
 import javax.servlet.ServletContextEvent
 import javax.servlet.ServletContextListener
+
 class StartupListener extends ServletContextListener {
 		
-	def contextDestroyed(event : ServletContextEvent){
-		
-	}
+	def contextDestroyed(event : ServletContextEvent) {}
 	
 	def contextInitialized(event : ServletContextEvent) {
 		new Thread(new GameThread).start
