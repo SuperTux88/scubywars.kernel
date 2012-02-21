0e0281463a26217260f554767dfb862259f57aa1
Benjamin Neff <benjamin@coding4coffee.ch>
2012-02-21 01:40:00 +0100

    reply...

--- src/main/scala/de/tdng2011/game/kernel/IdActor.scala
+++ src/main/scala/de/tdng2011/game/kernel/IdActor.scala
@@ -3,16 +3,16 @@ package de.tdng2011.game.kernel
 import actors.Actor
 
 object IdActor extends Actor {
-  var publicIds = 0
-
-  start
+  var publicIds: Long = 0
 
   def act = {
     loop {
       react {
         case x => {
           publicIds += 1
-          publicIds
+          reply {
+            publicIds
+          }
         }
       }
     }
--- src/main/scala/de/tdng2011/game/kernel/Server.scala
+++ src/main/scala/de/tdng2011/game/kernel/Server.scala
@@ -9,42 +9,43 @@ package de.tdng2011.game.kernel
 object Server {
   val framesPerSecond = 40.0
 
-  val frameDuration = 1000.0/framesPerSecond
+  val frameDuration = 1000.0 / framesPerSecond
 
-  def main(args : Array[String]){
+  def main(args: Array[String]) {
 
+    IdActor.start
     World.start
     ScoreBoard.start
 
     if (args contains "--startDefaultBots") startDefaultBots
 
-    var lastSleepTime : Double = 0
+    var lastSleepTime: Double = 0
 
-    while(true){
-      val frameStart = getTime               // 8000
+    while (true) {
+      val frameStart = getTime // 8000
 
-      World !? ThinkMessage(lastSleepTime/1000.0, null)
+      World !? ThinkMessage(lastSleepTime / 1000.0, null)
 
-      val frameEnd = getTime                 // 8024  / 8050
-      val sleepTime =  (frameDuration) - (frameEnd - frameStart)
-      if(sleepTime > 0)                      //    1  /  -25
-        Thread sleep(sleepTime.toLong)
-      val afterSleep = getTime               // 8025  / 8050
-      lastSleepTime = afterSleep-frameStart  //   25  /   50
+      val frameEnd = getTime // 8024  / 8050
+      val sleepTime = (frameDuration) - (frameEnd - frameStart)
+      if (sleepTime > 0) //    1  /  -25
+        Thread sleep (sleepTime.toLong)
+      val afterSleep = getTime // 8025  / 8050
+      lastSleepTime = afterSleep - frameStart //   25  /   50
     }
 
     System exit 0
   }
 
-	def getTime = System.currentTimeMillis
+  def getTime = System.currentTimeMillis
 
   def startDefaultBots {
     new Thread() {
       override def run {
 
-         World !? AddPlayerMessage("1x")
-        (World !? AddPlayerMessage("2x")).asInstanceOf[Some[Player]].get !! PlayerActionMessage(false,false,true,true)
-        (World !? AddPlayerMessage("3x")).asInstanceOf[Some[Player]].get !! PlayerActionMessage(true,false,true,true)
+        World !? AddPlayerMessage("1x")
+        (World !? AddPlayerMessage("2x")).asInstanceOf[Some[Player]].get !! PlayerActionMessage(false, false, true, true)
+        (World !? AddPlayerMessage("3x")).asInstanceOf[Some[Player]].get !! PlayerActionMessage(true, false, true, true)
 
         new de.tdng2011.game.killerclient.Client("localhost", "##OJ##")
       }
